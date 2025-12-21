529e8dd032984dc2de5c15f944d20609114076a5
Vinramis <rmuravlev6@gmail.com>
2025-12-21 13:33:05 +0500

    sd

--- emis automator/cleaner.cmd
+++ emis automator/cleaner.cmd
@@ -2,6 +2,8 @@
 CHCP 65001 >nul
 setlocal enabledelayedexpansion
 
+:: === EDIT START ===
+
 set "PYTHON_PATH="python314""
 
 :: Delete unnecessary files
@@ -10,9 +12,12 @@ rd /s /q "%PYTHON_PATH%\Doc" 2>nul
 rd /s /q "%PYTHON_PATH%\tcl" 2>nul
 rd /s /q "%PYTHON_PATH%\Lib\test" 2>nul
 rd /s /q "%PYTHON_PATH%\Lib\idlelib" 2>nul
+rd /s /q "%PYTHON_PATH%\LICENSE.txt" 2>nul
 
 :: Delete all pycache folders recursively
 for /d /r "%PYTHON_PATH%" %%d in (__pycache__) do @if exist "%%d" rd /s /q "%%d"
+
+:: === EDIT END ===
 
 :: Close window
 echo.
