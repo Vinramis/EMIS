37237879d2e89c93124ac4035d2623ee45adb4f8
Vinramis <rmuravlev6@gmail.com>
2025-12-22 13:18:30 +0500

    sd

--- emis automator/СТАРТ.cmd
+++ emis automator/СТАРТ.cmd
@@ -2,33 +2,34 @@
 CHCP 65001 >nul
 setlocal enabledelayedexpansion
 
-:: --- Activate fullscreen mode ---
+:: Activate fullscreen mode
 if not "%1"=="max" start /MAX cmd /c %0 max & exit/b
 TITLE Автоматизатор EMIS v0.6.5
 
-:: --- Welcome ---
+:: Welcome
 echo.
 echo.
-echo. & TIMEOUT /T 1 >nul
-echo === Настройка автоматизатора EMIS ===
-echo. & TIMEOUT /T 1 >nul
-echo Этот скрипт поможет настроить параметры для автоматизации, затем запустит её
+echo.
+echo === Добро пожаловать в Автоматизатор EMIS! ===
+echo.
 echo.
 echo.
 
-:: --- Update dependencies ---
+:: Update dependencies
 echo. & TIMEOUT /T 1 >nul
 echo ---------------------------------------------------
 echo Обновление компонентов в отдельном окне...
+TIMEOUT /T 1 >nul
 echo (?) Если вы закроете окно, на появившийся вопрос ответьте "N"
+TIMEOUT /T 1 >nul
 start /wait dependencies_installer.cmd
 if errorlevel 0 (
     echo [ИНФО] Компоненты обновлены успешно
 ) else (
     echo [ИНФО] Компоненты не обновлены
 )
-echo. & TIMEOUT /T 1 >nul
+TIMEOUT /T 1 >nul
 
-:: --- Setup ---
-:: To be separated into a different file
+:: Setup
+::     To be separated into a different file
 setup_automator.cmd
