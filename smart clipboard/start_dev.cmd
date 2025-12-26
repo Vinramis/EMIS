e885639d53e4dc03966ab1e4ed7ca3b15ecb29e0
Vinramis <rmuravlev6@gmail.com>
2025-12-26 19:32:55 +0500

    unrelated project excluded

--- smart clipboard/start_dev.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-CHCP 65001 >nul
-title СТАРТ ПРО - Буфер Обмена
-
-REM Запуск основного скрипта С аргументом 'pro'
-run_clipboard_script.cmd pro
--- smart clipboard/СТАРТ.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-CHCP 65001 >nul
-title СТАРТ - Буфер Обмена
-
-REM Запуск основного скрипта БЕЗ аргумента 'pro'
-run_clipboard_script.cmd
